a0ada810a6d9988ec26288d649e25eefd92f8e3f
jikuya <jikuya@cv.info.gifu-u.ac.jp>
2023-06-08 12:57:33 +0900

    Update files in redcaps folder

--- redcaps/__init__.py
+++ redcaps/__init__.py
@@ -1,10 +1,9 @@
+# Copyright (c) 2023 Keisuke Jikuya
+
 # Copyright (c) Karan Desai (https://kdexd.xyz), The University of Michigan.
 
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .id_downloader import RedditIdDownloader
+from .image_worker import _image_worker
 from .image_downloader import ImageDownloader
-from .info_downloader import RedditInfoDownloader
-
-__all__ = ["RedditIdDownloader", "RedditInfoDownloader", "ImageDownloader"]
