3a413750e0a7c18e3b6dc747f2c5f6b0a6e28178
jikuya <jikuya@cv.info.gifu-u.ac.jp>
2023-06-08 12:53:56 +0900

    delete and move

--- redcaps/__init__.py
+++ redcaps/__init__.py
@@ -3,4 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-__version__ = "1.0"
+from .id_downloader import RedditIdDownloader
+from .image_downloader import ImageDownloader
+from .info_downloader import RedditInfoDownloader
+
+__all__ = ["RedditIdDownloader", "RedditInfoDownloader", "ImageDownloader"]
